e1811fa5ffc22ba99d6ab801c7b7b0c4960f765b
stoleks <stoleks@hotmail.fr>
2022-07-06 15:55:20 +0200

    Utilisation de plus de fonctions pour rendre le code plus lisible

--- LP/09-conversion_electromecanique/champ_statorique_machine_reel.py
+++ LP/09-conversion_electromecanique/champ_statorique_machine_reel.py
@@ -29,7 +29,7 @@ plt.rcParams.update({'font.size': 18})
 
 # definition des bouttons réglables
 couleurAxe = 'white'
-taille = [1/3, 0.05]
+taille = (1/3, 0.05)
 x = 0.08
 y = 0.925
 espacement = 0.06
@@ -52,12 +52,6 @@ xmin = -5
 ymin = -5
 xmax = 5
 ymax = 5
-# coordonnées avec les marges
-marge = 1
-xmin0 = xmin - marge
-ymin0 = ymin - marge
-xmax0 = xmax + marge
-ymax0 = ymax + marge
 
 
 """
@@ -73,6 +67,36 @@ def enRadian (angle):
 def angleBobine (n, deltaTheta, N):
     return (n - (N - 1)/2) * deltaTheta
 
+# calcul la position d'une bobine décalée
+def position (n, deltaTheta, N, decalage):
+    theta = angleBobine (n, deltaTheta, N) + enRadian (decalage)
+    return (np.cos (theta), np.sin (theta))
+
+"""
+Fonctions de tracé générales
+"""
+# trace un cercle
+def traceCercle (figure, pos, rayon, couleur='white', rempli=True, epaisseur=.0):
+  cercle = patches.Circle (pos, radius=rayon, color=couleur, fill=rempli, linewidth=epaisseur)
+  figure.add_patch (cercle)
+
+# trace une fleche
+def traceFleche (figure, base, tete, largeur=1.0, couleur='black'):
+    taille = 0.1 * largeur
+    figure.arrow (base[0], base[1], tete[0], tete[1], shape='full', lw=largeur, head_width=taille, color=couleur)
+
+# affiche du texte
+def traceTexte (figure, pos, texte, couleur='black', vert="bottom", horiz="right"):
+    figure.text (pos[0], pos[1], texte, color=couleur, va=vert, ha=horiz)
+
+# initialise les axes d'une figure
+def initialiseAxes (figure, x, y):
+    figure.set_xlim (x[0], x[1])
+    figure.set_ylim (y[0], y[1])
+    figure.set_frame_on (False)
+    figure.xaxis.set_visible (False)
+    figure.yaxis.set_visible (False)
+
 
 """
 Fonctions pour tracer le moteur
@@ -80,89 +104,74 @@ Fonctions pour tracer le moteur
 # Pour tracer une encoche à bobine
 # r      : rayon rotor + entrefer
 # a      : taille encoche
-def traceEncoche (r, a, cosTheta, sinTheta, figure):
-    # calcul position de l'encoche
+def traceEncoche (figure, r, a, pos):
+    # calcule la position de l'encoche et la trace
     rc = r + a/2
-    encochePos = (rc*cosTheta, rc*sinTheta)
-    # dessine un cercle blanc
-    encoche = patches.Circle (encochePos, radius=1.1*a, color='white', fill=True)
-    figure.add_patch (encoche)
+    encochePos = (rc*pos[0], rc*pos[1])
+    traceCercle (figure, encochePos, 1.1*a)
 
 # Pour tracer le fil d'une bobine entrant ou sortant
-def traceBobine (r, a, cosTheta, sinTheta, sortant, couleur, figure):
+def traceBobine (figure, r, a, pos, couleur, sortant):
     # dessine le fil
     rc = r + a/2
-    filPos = (rc*cosTheta, rc*sinTheta)
-    fil = patches.Circle (filPos, radius=a, color='black', linewidth=0.5, fill=False)
-    figure.add_patch (fil)
+    filPos = (rc*pos[0], rc*pos[1])
+    traceCercle (figure, filPos, a, 'black', False, .75)
     # dessine son sens
     if sortant :
-      sens = patches.Circle (filPos, radius=a/3, color=couleur, fill=True)
-      figure.add_patch (sens)
+      traceCercle (figure, filPos, a/2, couleur)
     else :
       lMin = r - a/4
       l = lMin + 3*a/4
       lMax = lMin + 7*a/4
       # trait vertical
-      croixV = [[lMin*cosTheta, lMax*cosTheta], [lMin*sinTheta, lMax*sinTheta]]
+      croixV = ([lMin*pos[0], lMax*pos[0]], [lMin*pos[1], lMax*pos[1]])
       figure.plot (croixV[0], croixV[1], color=couleur, linewidth=1.0)
       # trait horizontal
-      croixH = [[l*cosTheta - 0.9*a*sinTheta, l*cosTheta + 0.9*a*sinTheta],
-          [l*sinTheta + 0.9*a*cosTheta, l*sinTheta - 0.9*a*cosTheta]]
+      croixH = ([l*pos[0] - 0.9*a*pos[1], l*pos[0] + 0.9*a*pos[1]],
+          [l*pos[1] + 0.9*a*pos[0], l*pos[1] - 0.9*a*pos[0]])
       figure.plot (croixH[0], croixH[1], color=couleur, linewidth=1.0)
 
 # pour tracer le stator et le rotor
-def traceMachine (r, couleur, figure):
-    # trace le stator
-    stator = plt.Circle ((0, 0), r, color=couleur, fill=True)
-    figure.add_patch (stator)
-    # trace l'entrefer
-    entrefer = plt.Circle ((0, 0), 3*r/4, color='white', fill=True)
-    figure.add_patch (entrefer)
-    # trace le rotor
-    rotor = plt.Circle ((0, 0), 2*r/3, color=couleur, fill=True)
-    figure.add_patch (rotor)
+def traceMachine (fig, r, couleur):
+    # trace le stator, l'entrefer et le rotor
+    traceCercle (fig, (0, 0), r, couleur, True)
+    traceCercle (fig, (0, 0), 3*r/4)
+    traceCercle (fig, (0, 0), 2*r/3, couleur)
 
 # trace un bobinage
-def traceBobinage (r, a, i, deltaTheta, N, decalage, sortant, couleur, figure):
-    theta = angleBobine (i, deltaTheta, N) + enRadian (decalage)
-    traceEncoche (r, a, np.cos(theta), np.sin(theta), figure)
-    traceBobine (r, a, np.cos(theta), np.sin(theta), sortant, couleur, figure)
+def traceBobinage (fig, r, a, pos, couleur, sortant=True):
+    traceEncoche (fig, r, a, pos)
+    traceBobine (fig, r, 0.9*a, pos, couleur, sortant)
 
 # trace le moteur synchrone
-def traceMoteurSynchrone (N, deltaTheta, triphasee, figure):
-    figure.clear ()
-    # Zone de tracé de la visualisation du train et du tunnel
-    figure.set_xlim (-3, 3)
-    figure.set_ylim (-3, 3)
-    figure.set_frame_on (False)
-    figure.xaxis.set_visible (False)
-    figure.yaxis.set_visible (False)
-    figure.set_aspect ('equal')
-    figure.text (-.7, -3, r'$\alpha_{max}=$'+f'{0 : 3.1f}'+'°', color='black', va="top", ha="left")
+def traceMoteurSynchrone (N, deltaTheta, triphasee, fig):
+    fig.clear ()
+    # Zone de tracé
+    initialiseAxes (fig, (-3, 3), (-3, 3))
+    fig.set_aspect ('equal')
+    traceTexte (fig, (-.7, -3), r'$\alpha_{max}=$' + f'{0 : 3.1f}' + '°', vert="top", horiz="left")
 
     # trace la machine
-    traceMachine (2.9, 'grey', figure)
+    traceMachine (fig, 2.9, 'grey')
     r = 2.3
     decalage = 0.1
     a = decalage - 0.001*N
     couleur='dodgerblue'
     # premier jeu de bobine
     for i in range (0, N):
-        traceBobinage (r, a, i, deltaTheta, N,  90, True, couleur, figure)
-        traceBobinage (r, a, i, deltaTheta, N, 270, False, couleur, figure)
-    # second et troisieme jeu de bobine
+        traceBobinage (fig, r, a, position (i, deltaTheta, N,  90), couleur)
+        traceBobinage (fig, r, a, position (i, deltaTheta, N, 270), couleur, False)
     if triphasee:
         # second jeu
         couleur='crimson'
         for i in range (0, N):
-            traceBobinage (r + 2.1*decalage, a, i, deltaTheta, N, 210, True, couleur, figure)
-            traceBobinage (r + 2.1*decalage, a, i, deltaTheta, N,  30, False, couleur, figure)
+            traceBobinage (fig, r + 2.1*decalage, a, position (i, deltaTheta, N, 210), couleur)
+            traceBobinage (fig, r + 2.1*decalage, a, position (i, deltaTheta, N,  30), couleur, False)
         # troisieme jeu
         couleur='limegreen'
         for i in range (0, N):
-            traceBobinage (r + 4.2*decalage, a, i, deltaTheta, N, 330, True, couleur, figure)
-            traceBobinage (r + 4.2*decalage, a, i, deltaTheta, N, 150, False, couleur, figure)
+            traceBobinage (fig, r + 4.2*decalage, a, position (i, deltaTheta, N, 330), couleur)
+            traceBobinage (fig, r + 4.2*decalage, a, position (i, deltaTheta, N, 150), couleur, False)
 
 
 """
@@ -186,8 +195,7 @@ def traceVecteurChamp (t, r, l, triphasee, figure):
             cost = np.cos (angle) * np.cos (deuxPi*t)
             sint = np.sin (angle) * np.cos (deuxPi*t)
         ri = 3*r/4
-        figure.arrow (ri*cosi, ri*sini, l*cost*cosi, l*cost*sini,
-            shape='full', lw=1.0, length_includes_head=False, head_width=.1, color='black')
+        traceFleche (figure, (ri*cosi, ri*sini), (l*cost*cosi, l*cost*sini))
     # trace le champ total et affiche son angle
     base = [-r/2 * np.cos (deuxPi * t), 0]
     tete = [r * np.cos (deuxPi * t), 0]
@@ -196,8 +204,7 @@ def traceVecteurChamp (t, r, l, triphasee, figure):
         base[1] = -r/2 * np.sin (deuxPi * t)
         tete[1] = r * np.sin (deuxPi * t)
         texteAngle = f'{360 * t : 3.1f}'
-    figure.arrow (base[0], base[1], tete[0], tete[1],
-        shape='full', lw=1.3, length_includes_head=True, head_width=.3, color='midnightblue')
+    traceFleche (figure, base, tete, 1.3, 'midnightblue')
     figure.texts[0].set_text (r'$\alpha_{max}=$' + texteAngle + '°')
 
 # initialise le diagramme du champ
@@ -209,23 +216,16 @@ def initialiseDiagramme (figure):
 # trace les axes du diagrammes B(theta)
 def traceAxes (figure):
     # zone de tracé du diagramme
-    figure.set_xlim (xmin0, xmax0)
-    figure.set_ylim (ymin0, ymax0)
-    figure.set_frame_on (False)
-    figure.xaxis.set_visible (False)
-    figure.yaxis.set_visible (False)
+    m = 1 # marge
+    initialiseAxes (figure, (xmin - m, xmax + m), (ymin - m, ymax + m))
     # paramètres des axes
     taille = 0.4
     # axe theta
-    figure.arrow (xmin - taille, 0, xmax - xmin + 2*taille, 0,
-        shape='full', lw=.75, length_includes_head=True, head_width=.25, color='black')
-    figure.text (xmax + taille, taille/2, r'$\alpha$',
-        color='black', va="bottom", ha="right")
+    traceFleche (figure, (xmin - taille, 0), (xmax - xmin + 2*taille, 0))
+    traceTexte (figure, (xmax + taille, taille/2), r'$\alpha$')
     # axe B
-    figure.arrow (0, ymin - taille, 0, ymax - ymin + 3*taille,
-        shape='full', lw=.75, length_includes_head=True, head_width=.25, color='black')
-    figure.text (taille/2, ymax + 2*taille, "$B_s$",
-        color='black', va="bottom", ha="right")
+    traceFleche (figure, (0, ymin - taille), (0, ymax - ymin + 3*taille))
+    traceTexte (figure, (taille/2, ymax + 2.4*taille), '$B_s$')
 
 
 """
